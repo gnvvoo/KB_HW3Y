cb494ac9eb130c2b06cf665cfa12d07f63eab9cd
gnvvoo <t412@naver.com>
2026-04-08 14:21:13 +0900

    feat: 계정 정보 입력 화면 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RegisterView from '../views/RegisterView.vue'
+import RegisterAgreement from '../components/Register/RegisterAgreement.vue'
+import RegisterAccount from '../components/Register/RegisterAccount.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/register',
+      component: RegisterView,
+      children: [
+        // /register 접근 시 1단계(약관 동의)로 리다이렉트
+        { path: '', redirect: '/register/agreement' },
+        { path: 'agreement', component: RegisterAgreement },
+        { path: 'account', component: RegisterAccount },
+      ],
+    },
+  ],
 })
 
 export default router
